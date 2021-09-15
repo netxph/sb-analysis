c627ad042f44a152044c8db71c5e1d801aa15488
Marc Vitalis <marc.vitalis@live.com>
2021-09-15 17:32:15 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Libraries for Starbucks Portfolio Exercise",
+    packages=["src"],
+    url="https://github.com/netxph/sb-analysis",
+    author="Marc Vitalis"
+)
